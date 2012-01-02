ffd919a85a50775c33ce14ec4cb0a08e594fc917
Bryan MacFarlane <bryanmacf@gmail.com>
2012-01-01 21:05:30 -0500

    breakout server into a module

--- helloworld/server.js
+++ helloworld/server.js
@@ -1,30 +1,40 @@
 /**
  *
- * Basic node http server
+ * Basic node http server module
  *
  * Created : bryan
  * Date    : 1/1/12
  */
 
+// create the http module
+// best practice to name variable the same as module
 var http = require("http");
-console.log("http created");
+console.log("http module created.");
 
-var server = http.createServer
-    (
-        function(request, response)
-        {
-            response.writeHead(200, {"Content-Type": "text/plain"});
-            response.write("Hello World");
-            response.end();
-        }
-    );
-console.log("server created");
+function start()
+{
+    console.log("server::start");
+    var server = http.createServer
+        (
+            function(request, response)
+            {
+                // will print twice because of request for favicon.ico
+                console.log("request received: " + request.url);
+                response.writeHead(200, {"Content-Type": "text/plain"});
+                response.write("Hello World!");
+                response.end();
+            }
+        );
+    console.log("server created.");
 
-var port = 8888;
-server.listen(port);
+    var port = 8888;
+    server.listen(port);
 
-console.log("server listening on port: " + port);
+    console.log("server listening on port: " + port);
+}
 
+// our module offers (exports) one start method
+exports.start = start;
 
 
 
